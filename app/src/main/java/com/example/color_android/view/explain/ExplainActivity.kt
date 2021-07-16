de76bb10aec88053411534c29d59552bad565c1f
Anbyeonghun <abh0920one@gmail.com>
2021-07-17 00:37:48 +0900

    [CREATE] angry explain

---
+++ app/src/main/java/com/example/color_android/view/explain/ExplainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.color_android.view.explain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.color_android.R
+
+class ExplainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explain)
+
+
+    }
+}
