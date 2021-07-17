d899415efc687c57f705bbf9695258556c5ee5ac
Anbyeonghun <abh0920one@gmail.com>
2021-07-17 16:51:01 +0900

    [CREATE] Angry feed ui

---
+++ app/src/main/java/com/example/color_android/view/angry/AngryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.color_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AngryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_angry)
+    }
+}
