18cab21b14609dee4d5aabf9ac5054bfb79b5763
Anbyeonghun <abh0920one@gmail.com>
2021-07-18 15:27:42 +0900

    [CREATE] Write Activity ui

---
+++ app/src/main/java/com/example/color_android/view/write/WriteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.color_android.view.write
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.color_android.R
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
