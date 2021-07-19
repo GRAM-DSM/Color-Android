6f9c34538fcfb73aa4a05a3416dc8e5887c0c21f
Anbyeonghun <abh0920one@gmail.com>
2021-07-20 01:24:53 +0900

    [UPDATE] LoginRepository setToken function

--- app/src/main/java/com/example/color_android/data/repository/LoginRepository.kt
+++ app/src/main/java/com/example/color_android/data/repository/LoginRepository.kt
@@ -1,10 +1,17 @@
 package com.example.color_android.data.repository
 
 import com.example.color_android.data.LoginRequest
+import com.example.color_android.data.LoginResponse
 import com.example.color_android.network.RetrofitClient
+import com.example.color_android.util.SharedPreferencesHelper
 
 class LoginRepository : BaseRepository() {
     suspend fun login(body: LoginRequest) = safeApiCall {
         RetrofitClient.getAPI().login(body)
+    }
+
+    fun setToken(tokenResponse: LoginResponse){
+        SharedPreferencesHelper.getInstance().access_token = "Bearer " + tokenResponse.access_token
+        SharedPreferencesHelper.getInstance().refresh_token = "Bearer " + tokenResponse.refresh_token
     }
 }
