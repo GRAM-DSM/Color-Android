b0b1d789aed41b3cf930a61bf5e67840baefcad4
Anbyeonghun <abh0920one@gmail.com>
2021-07-22 14:44:01 +0900

    [CREATE] SignRepository

---
+++ app/src/main/java/com/example/color_android/data/repository/sign/SignRepository.kt
@@ -0,0 +1,21 @@
+package com.example.color_android.data.repository.sign
+
+import com.example.color_android.data.model.sign.EmailCertifyRequest
+import com.example.color_android.data.model.sign.LoginRequest
+import com.example.color_android.data.model.sign.LoginResponse
+import com.example.color_android.data.model.sign.RegisterRequest
+import retrofit2.Response
+
+interface SignRepository {
+    suspend fun login(body: LoginRequest)
+
+    fun setToken(tokenResponse: LoginResponse)
+
+    suspend fun register(body: RegisterRequest) : Response<Void>
+
+    suspend fun nicknameCheck(body: HashMap<String, String>) : Response<Void>
+
+    suspend fun sendEmail(body: HashMap<String, String>) : Response<Void>
+
+    suspend fun emailCertify(body: EmailCertifyRequest) : Response<Void>
+}
---
+++ app/src/main/java/com/example/color_android/data/repository/sign/SignRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package com.example.color_android.data.repository.sign
+
+import com.example.color_android.data.model.sign.EmailCertifyRequest
+import com.example.color_android.data.model.sign.LoginRequest
+import com.example.color_android.data.model.sign.LoginResponse
+import com.example.color_android.data.model.sign.RegisterRequest
+import com.example.color_android.network.RetrofitClient
+import com.example.color_android.network.SafeApiRequest
+import com.example.color_android.util.SharedPreferencesHelper
+import retrofit2.Response
+
+class SignRepositoryImpl : SignRepository, SafeApiRequest() {
+    override suspend fun login(body: LoginRequest) {
+        RetrofitClient.getAPI().login(body)
+    }
+
+    override fun setToken(tokenResponse: LoginResponse){
+        SharedPreferencesHelper.getInstance().access_token = "Bearer " + tokenResponse.access_token
+        SharedPreferencesHelper.getInstance().refresh_token = "Bearer " + tokenResponse.refresh_token
+    }
+
+    override suspend fun register(body: RegisterRequest): Response<Void> {
+        return safeApiCall { RetrofitClient.getAPI().register(body) }
+    }
+
+    override suspend fun nicknameCheck(body: HashMap<String, String>): Response<Void> {
+        return safeApiCall { RetrofitClient.getAPI().nicknameCheck(body) }
+    }
+
+    override suspend fun sendEmail(body: HashMap<String, String>): Response<Void> {
+        return safeApiCall { RetrofitClient.getAPI().sendEmail(body) }
+    }
+
+    override suspend fun emailCertify(body: EmailCertifyRequest): Response<Void> {
+        return safeApiCall { RetrofitClient.getAPI().emailCertify(body) }
+    }
+}
