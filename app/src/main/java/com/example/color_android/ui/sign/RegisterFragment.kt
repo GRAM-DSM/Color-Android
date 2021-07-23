500e2b35d5dbddb69ce5460516588c39d8442a25
Anbyeonghun <abh0920one@gmail.com>
2021-07-23 12:21:16 +0900

    [UPDATE] Finish RegisterViewModel implements

--- app/src/main/java/com/example/color_android/ui/sign/RegisterFragment.kt
+++ app/src/main/java/com/example/color_android/ui/sign/RegisterFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.color_android.R
+import com.example.color_android.network.RegisterSet
 import com.example.color_android.viewmodel.RegisterViewModel
 import kotlinx.android.synthetic.main.fragment_register.*
 
@@ -25,12 +27,24 @@ class RegisterFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        registerViewModel.registerLiveData.observe(viewLifecycleOwner, {
+            when(it){
+                RegisterSet.NameSuccess -> Toast.makeText(activity, "Success", Toast.LENGTH_SHORT).show()
+                RegisterSet.SendEmailSuccess -> Toast.makeText(activity, "Send Email", Toast.LENGTH_SHORT).show()
+            }
+        })
+
         sign_close_ib.setOnClickListener{
             (activity as SignActivity).replaceFragment(LoginFragment())
         }
 
         sign_overlap_tv.setOnClickListener{
             registerViewModel.nameCheck(sign_name_et.text.toString())
+        }
+
+        sign_send_tv.setOnClickListener{
+            registerViewModel.sendEmail(sign_email_et.text.toString())
         }
     }
 }
--- app/src/main/java/com/example/color_android/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/example/color_android/viewmodel/RegisterViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.color_android.data.model.sign.EmailCertifyRequest
 import com.example.color_android.data.model.sign.RegisterRequest
 import com.example.color_android.data.repository.sign.SignRepositoryImpl
 import com.example.color_android.network.RegisterSet
@@ -14,13 +15,6 @@ class RegisterViewModel : ViewModel() {
     private val signRepository = SignRepositoryImpl()
     private val _registerLiveData : MutableLiveData<RegisterSet> = MutableLiveData()
     val registerLiveData : LiveData<RegisterSet> = _registerLiveData
-
-//    fun register(body: RegisterRequest){
-//        viewModelScope.launch {
-//            val response = signRepository.register(body)
-//            if(response.isSuccessful)
-//        }
-//    }
 
     fun nameCheck(nickname : String){
         val body = HashMap<String, String>()
@@ -36,11 +30,70 @@ class RegisterViewModel : ViewModel() {
         }
     }
 
+    fun sendEmail(email : String){
+        val body = HashMap<String, String>()
+        body["email"] = email
+        viewModelScope.launch {
+            val response = signRepository.sendEmail(body)
+            if(response.isSuccessful){
+                sendEmailSuccess(response)
+            } else{
+                _registerLiveData.postValue(RegisterSet.SendEmailFail)
+            }
+        }
+    }
+
+    fun emailCertify(emailCertifyRequest: EmailCertifyRequest){
+        viewModelScope.launch {
+            val response = signRepository.emailCertify(emailCertifyRequest)
+            if(response.isSuccessful){
+                emailCertifySuccess(response)
+            } else {
+                _registerLiveData.postValue(RegisterSet.EmailFail)
+            }
+        }
+    }
+
+    fun register(registerRequest: RegisterRequest){
+        viewModelScope.launch {
+            val response = signRepository.register(registerRequest)
+            if(response.isSuccessful){
+                registerSuccess(response)
+            } else {
+                _registerLiveData.postValue(RegisterSet.RegisterFail)
+            }
+        }
+    }
+
     private fun nameCheckSuccess(response: Response<Void>){
         if(response.code() == 200){
             _registerLiveData.postValue(RegisterSet.NameSuccess)
         } else {
             _registerLiveData.postValue(RegisterSet.NameFail)
+        }
+    }
+
+    private fun sendEmailSuccess(response: Response<Void>){
+        if(response.code() == 200){
+            _registerLiveData.postValue(RegisterSet.SendEmailSuccess)
+        } else {
+            _registerLiveData.postValue(RegisterSet.SendEmailFail)
+        }
+    }
+
+    private fun emailCertifySuccess(response: Response<Void>){
+        if(response.code() == 200){
+            _registerLiveData.postValue(RegisterSet.EmailSuccess)
+        } else {
+            _registerLiveData.postValue(RegisterSet.EmailFail)
+        }
+    }
+
+    private fun registerSuccess(response: Response<Void>){
+        if(response.code() == 201){
+            _registerLiveData.postValue(RegisterSet.RegisterSuccess)
+        } else {
+            _registerLiveData.postValue(RegisterSet.RegisterFail)
         }
     }
 }
