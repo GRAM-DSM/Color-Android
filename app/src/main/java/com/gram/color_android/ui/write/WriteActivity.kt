c57f3c3d54f07488f50482e29b0fc9db3ed77c24
Anbyeonghun <abh0920one@gmail.com>
2021-08-11 20:25:03 +0900

    [CREATE] WriteActivity null check & Scrollview resize

--- app/src/main/java/com/gram/color_android/ui/write/WriteActivity.kt
+++ app/src/main/java/com/gram/color_android/ui/write/WriteActivity.kt
@@ -4,37 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.gram.color_android.R
 import com.gram.color_android.databinding.ActivityWriteBinding
 import com.gram.color_android.network.set.FeelSet
+import com.gram.color_android.viewmodel.WriteViewModel
 import kotlinx.android.synthetic.main.activity_write.*
-import org.w3c.dom.Text
 
 class WriteActivity : AppCompatActivity() {
 
+    private lateinit var dataBinding: ActivityWriteBinding
+    private val writeViewModel = WriteViewModel()
+    private var feel: FeelSet? = null
+    private var curView: TextView? = null
 
-    private var feel : FeelSet? = null
-    private var curView : TextView? = null
-    private lateinit var dataBinding : ActivityWriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_write)
         dataBinding.lifecycleOwner = this
         dataBinding.activity = this
-        
+
+        write_post_btn.setOnClickListener{
+            if(nullCheck())
+                Toast.makeText(this, "asdasdasda", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    fun selectFeel(tv : TextView, feel: FeelSet){
-        if(curView == null) {
+    fun selectFeel(tv: TextView, feel: FeelSet) {
+        if (curView == null) {
             curView = tv
             curView!!.visibility = View.VISIBLE
-        }
-        else {
+        } else {
             curView!!.visibility = View.GONE
             curView = tv
             curView!!.visibility = View.VISIBLE
         }
         this.feel = feel
+        when(feel){
+            FeelSet.ANGRY -> write_post_btn.setBackgroundColor(getColor(R.color.angry))
+            FeelSet.HAPPY -> write_post_btn.setBackgroundColor(getColor(R.color.happy))
+            FeelSet.SAD -> write_post_btn.setBackgroundColor(getColor(R.color.sad))
+            FeelSet.BORED -> write_post_btn.setBackgroundColor(getColor(R.color.bored))
+            FeelSet.LOVE -> write_post_btn.setBackgroundColor(getColor(R.color.love))
+            FeelSet.SHAME -> write_post_btn.setBackgroundColor(getColor(R.color.shame))
+        }
     }
+
+    private fun nullCheck() = (feel != null && !write_content_et.text.toString().equals("")
+            && !write_tag_et.text.toString().equals(""))
 }
